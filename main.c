a4e4f40227fbd028db85a50181f308c1af91735a
Raphaël <xelaphar@gmail.com>
2024-01-08 11:02:41 +0100

    modif

--- main.c
+++ main.c
@@ -1,21 +1,16 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <assert.h>
+
+struct cellule_s {
+    char data;
+    struct cellule_s* suiv;
+};
+typedef struct cellule_s cellule ;
+typedef cellule* dico ;
+
+
+
 
 int main(int argc, char* argv[]){
-
-    assert ( argc == 2) ;
-    errno = 0;
-    FILE * fd = fopen ( argv [1] , " r " ) ;
-    if ( fd == NULL ) {
-    printf ( " Erreur lors de l ' ouverture du fichier % s : % d (% s ) \ n " ,
-    argv [1] , errno , strerror ( errno ) ) ;
-    return 1;
-    }
-
-    int nb = 0;
-    fscanf ( fd , " % d " , & nb ) ;
-    fclose ( fd );
 
     return 0;
 }
