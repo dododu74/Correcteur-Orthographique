789bb45c7da44d1e534fb2d83d11f0d86571458b
Dorian J <dodojosse74@gmail.com>
2024-01-08 10:53:27 +0100

    Rangements

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <assert.h>
 
 int main(int argc, char* argv[]){
+
+    assert ( argc == 2) ;
+    errno = 0;
+    FILE * fd = fopen ( argv [1] , " r " ) ;
+    if ( fd == NULL ) {
+    printf ( " Erreur lors de l ' ouverture du fichier % s : % d (% s ) \ n " ,
+    argv [1] , errno , strerror ( errno ) ) ;
+    return 1;
+    }
+
+    int nb = 0;
+    fscanf ( fd , " % d " , & nb ) ;
+    fclose ( fd );
 
     return 0;
 }
