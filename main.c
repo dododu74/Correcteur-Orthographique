51f7dd1ae3de9c926e46fd2c4093f0e3ff40efbb
Raphaël <xelaphar@gmail.com>
2024-01-08 11:49:36 +0100

    string -> char*

--- main.c
+++ main.c
@@ -19,7 +19,7 @@ dico dictionnaire_vide(void) {
 }
 
 
-bool chercher_mot(dico dic,string mot){
+bool chercher_mot(dico dic,char* mot){
     if dic=NULL return false;
     if mot='' return dic.isterminal ;
     next=dico[mot[i]-'a'].suiv;
